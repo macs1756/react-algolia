1e71b6e3ef96d35c7674ff97d9c6f3238e43cf3a
macs1756 <kindrativmaksum312@gmail.com>
2024-03-10 13:04:12 +0200

    v13:04

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
-	
+
 import {
+  Hits,
   InstantSearch,
   RefinementList,
   useInstantSearch,
 } from 'react-instantsearch';
+import algoliasearch from 'algoliasearch/lite';
 
+const searchClient = algoliasearch(
+  process.env.REACT_APP_ALGOLIA_APP_ID,
+  process.env.REACT_APP_ALGOLIA_SEARCH_KEY
+);
 
 function App() {
-  const { indexUiState, setIndexUiState } = useInstantSearch();
+  //const { indexUiState, setIndexUiState } = useInstantSearch();
 
   return (
     <div className="App">
       <h2>Algolia search:</h2>
+      <InstantSearch searchClient={searchClient} indexName="full">
+        {/* <Discounts /> */}
+        <RefinementList attribute="post_type" />
 
+        <RefinementList attribute="brand" />
+
+
+        <Hits />
+      </InstantSearch>
     </div>
   );
 }
