b709f1657d20732262d1b95c0f2fefda57431385
macs1756 <kindrativmaksum312@gmail.com>
2024-03-10 12:48:37 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+	
+import {
+  InstantSearch,
+  RefinementList,
+  useInstantSearch,
+} from 'react-instantsearch';
+
 
 function App() {
+  const { indexUiState, setIndexUiState } = useInstantSearch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Algolia search:</h2>
+
     </div>
   );
 }
