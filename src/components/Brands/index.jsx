cb42ee933b94559aaddbbb545afd297449b0dc26
macs1756 <kindrativmaksum312@gmail.com>
2024-03-10 18:45:36 +0200

    v18:45

--- src/components/Brands/index.jsx
+++ src/components/Brands/index.jsx
@@ -1,19 +1,13 @@
 import { useEffect, useState } from "react";
-import { useRefinementList } from "react-instantsearch";
+import { useInstantSearch, useRefinementList } from "react-instantsearch";
 
 
 function Brands(props) {
 
   const [allBrands, setAllBrands] = useState(null)
 
-  const {
-    items,
-    refine,
-    searchForItems,
-    canToggleShowMore,
-    isShowingMore,
-    toggleShowMore,
-  } = useRefinementList(props);
+  const { items } = useRefinementList(props);
+  const { indexUiState, setIndexUiState } = useInstantSearch();
 
   useEffect(() => {
     if (allBrands === null) {
@@ -22,8 +16,42 @@ function Brands(props) {
       if (allBrands.length < items.length) setAllBrands(items)
     }
 
-
   }, [items])
+
+
+  useEffect(() => {
+    console.log(indexUiState);
+  }, [indexUiState])
+
+  const changeFillters = (value, type) => {
+    if (type === 'add') {
+
+      console.log(value);
+
+      setIndexUiState((prevIndexUiState) => ({
+        ...prevIndexUiState,
+        refinementList: {
+          ...prevIndexUiState.refinementList,
+          brand: prevIndexUiState.refinementList.brand
+            ? [...prevIndexUiState.refinementList.brand, value]
+            : [value],
+        },
+      }));
+
+    } else {
+
+      setIndexUiState((prevIndexUiState) => ({
+        ...prevIndexUiState,
+        refinementList: {
+          ...prevIndexUiState.refinementList,
+          brand: prevIndexUiState.refinementList.brand
+            ? prevIndexUiState.refinementList.brand.filter(item => item !== value)
+            : [],
+        },
+      }));
+
+    }
+  }
 
   //console.log(currentType);
 
@@ -32,7 +60,10 @@ function Brands(props) {
       {
         allBrands && allBrands.map((e, i) => (
           <li key={e?.label + i} >
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={(event) => { changeFillters(e?.value, event.target.checked ? 'add' : 'remove') }}
+            />
             {e?.label}
           </li>
         ))
